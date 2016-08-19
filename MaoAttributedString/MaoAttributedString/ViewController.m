a406d766d304aba3ed5a3acd5ec8dde7abc64336
毛韶谦 <2802221460@qq.com>
2016-08-19 11:25:56 +0800

    富文本

--- MaoAttributedString/MaoAttributedString/ViewController.m
+++ MaoAttributedString/MaoAttributedString/ViewController.m
@@ -16,6 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UILabel *myFirstLabel = [[UILabel alloc] initWithFrame:CGRectMake(60, 100, 300, 30)];
+    [self.view addSubview:myFirstLabel];
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:@"MaoAttributedString蓝翔技校" attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:20]}];
+    [attributedString addAttributes:@{NSForegroundColorAttributeName:[UIColor redColor],NSBackgroundColorAttributeName:[UIColor blueColor]} range:NSMakeRange(0, 2)];
+    [attributedString addAttributes:@{NSStrikethroughStyleAttributeName:@(9)} range:NSMakeRange(2, 5)];
+    [attributedString addAttributes:@{NSStrikethroughStyleAttributeName:@(2),NSStrikethroughColorAttributeName:[UIColor greenColor]} range:NSMakeRange(9, 3)];
+    [attributedString addAttributes:@{NSStrokeColorAttributeName:[UIColor orangeColor],NSStrokeWidthAttributeName:@(2),NSFontAttributeName:[UIFont systemFontOfSize:30]} range:NSMakeRange(13, 4)];
+    [attributedString addAttributes:@{NSStrokeColorAttributeName:[UIColor orangeColor],NSStrokeWidthAttributeName:@(-2),NSFontAttributeName:[UIFont systemFontOfSize:25]} range:NSMakeRange(17, 3)];
+    
+    myFirstLabel.attributedText = attributedString;
+    
+    UITextField *myFirstTextField = [[UITextField alloc] initWithFrame:CGRectMake(60, 200, 300, 30)];
+    [self.view addSubview:myFirstTextField];
+    
+    
+    UITextView *myFirstTextView = [[UITextView alloc] initWithFrame:CGRectMake(60, 300, 300, 30)];
+    [self.view addSubview:myFirstTextView];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
